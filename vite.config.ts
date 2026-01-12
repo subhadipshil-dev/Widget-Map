d8751cdd20c0185a9463fd62675a6f8bd986de99
subhadipshil-dev <subhadipshil.dev@gmail.com>
2026-01-13 04:06:46 +0530

    Fix: Use docs folder for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/widget-map/',
+  build: {
+    outDir: 'docs'
+  },
   server: {
     port: 3000,
     open: true
